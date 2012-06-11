d067a52e9a8f5166f05b667cf85c7d4789aae9ca
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 14:54:26 +0200

    Amended for use on lucid, precise.
    Note: This is not entirely complete yet for precise; we are missing
    the heimdal dependency subtree. However, the required lines are
    present and commented out in metadata.rb and recipes/default.rb.
    
    The heimdal library tree will require another 9 cookbooks o,O
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,19 @@
+include_recipe "libgnutls"
+include_recipe "libsasl"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libgssapi-krb5"
+
   packages |= %w/
     libldap-2.4-2
   /
 when "precise"
+  include_recipe "libgcrypt"
+  #include_recipe "libgssapi3-heimdal"
+
   packages |= %w/
     libldap-2.4-2
   /
