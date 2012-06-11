961bb79f14f3c73300ac4be0886c38a102bb8dfe
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 14:52:15 +0200

    Added cookbook dependencies.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libgcrypt", ">= 0.0.1"
+depends          "libgnutls", ">= 0.0.1"
+depends          "libgssapi-krb5", ">= 0.0.1"
+#depends          "libgssapi3-heimdal", ">= 0.0.1"
+depends          "libsasl", ">= 0.0.1"
